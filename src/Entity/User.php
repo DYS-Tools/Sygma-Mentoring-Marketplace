a8d833c5a99718b1b59ba93675765f874d2020a5
Yohann Durand <yohanndurand76@gmail.com>
2021-06-21 18:33:00 +0200

    [Fix bug] add user registration, DataFixtures and update navbar

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,16 +3,17 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Proxies\__CG__\App\Entity\User as EntityUser;
+
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -63,6 +64,17 @@ class User
 
         return $this;
     }
+    
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
 
     /**
      * A visual identifier that represents this user.
