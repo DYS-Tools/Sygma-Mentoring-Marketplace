de10365d2787a65b2795bf45e16bed5248e220f8
Sacha Durand <sacha6623@gmail.com>
2021-09-27 17:50:20 +0200

    design(user): service in dashboard, payment template

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,21 @@ class AccountController extends AbstractController
     /**
      * @Route("/account", name="app_account")
      */
-    public function index(): Response
+    public function index(ServiceRepository $serviceRepository): Response
     {
+        $user = $this->getUser();
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+            'services' => $serviceRepository->findBy(['user' => $user]),
+        ]);
+    }
+
+    /**
+     * @Route("/account/payment", name="account_payment")
+     */
+    public function accountPayment(): Response
+    {
+        return $this->render('account/paymentSetting.html.twig', [
         ]);
     }
 }
